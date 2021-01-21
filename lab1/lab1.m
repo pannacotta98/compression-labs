28d01ba5f7f5ed4dca5e3029eb4797c0ff2c2e0b
Algot Sandahl <algot.sandahl@gmail.com>
2021-01-21 19:11:07 +0100

    Almost nothing

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -1,3 +1,4 @@
+%% ===== AUDIO =====
 hey04  = 128 * audioread('hey04_8bit.wav');
 nuit04 = 128 * audioread('nuit04_8bit.wav');
 speech = 128 * audioread('speech.wav');
@@ -6,3 +7,15 @@ x = hey04;
 [counts, bins] = intHistogram(x, -128, 127);
 
 probs = counts ./ length(x);
+
+%% Pairs
+
+pairCounts = zeros(length(bins) - 1);
+
+for i = 1:length(x)
+   for j = 1:length(x)
+       
+   end
+end
+
+%% ===== IMAGES =====
