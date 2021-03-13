36b87a7c3c8accc3925505cfe1bad58b98ec943a
Algot Sandahl <algot.sandahl@gmail.com>
2021-03-13 10:37:17 +0100

    More lab 2

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -1,13 +1,16 @@
+clear
+format compact
 %% Parameters
 clear
 % Typical block sizes for audio coding are 256--2048 sample // Slides
 windowSize = 512;
-quantSteps = 24;
+quantSteps = 24.3;
 
 %% Read data
 [x, Fs] = audioread('audio/heyhey.wav');
 
 stepSize = (max(x) - min(x)) / quantSteps;
+maxRate = 128000 / Fs
 
 %% Compression
 xTransf = mdct(x, windowSize);
@@ -17,6 +20,7 @@ quantizedTransf = uniformQuantization(xTransf, stepSize);
 xRec = imdct(quantizedTransf * stepSize);
 player = audioplayer(xRec, Fs);
 play(player)
+audiowrite('compressedReconstructed.wav', xRec, Fs);
 
 %% Calculate stats
 SNR = signalToNoise(x, xRec)
@@ -24,6 +28,13 @@ SNR = signalToNoise(x, xRec)
 quantHist = ihist(quantizedTransf(:));
 probs = quantHist / sum(quantHist);
 R = huffman(probs)
+
+N = length(quantHist)
+lengthsData = N * ceil(log2(N - 1))
+
+%stepSizeIn16Bit = 0.5 * (2^16) * stepSize
+
+RWithSideInfo = R + lengthsData / length(x)
 
 %% Stop audio
 clear player % ?
